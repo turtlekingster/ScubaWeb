d8f915caec7af154eb14a7341b3b0cb0ee48cb27
Justin <turtlekingster@gmail.com>
2017-04-01 02:53:44 -0400

    Worked on DOMTravler Problems with tree traversal fixed indents

--- interface/test2.py
+++ interface/test2.py
@@ -13,14 +13,14 @@ class WebView(QWebView):
         QWebView.__init__(self)
         self.newBranch = QtGui.QAction('Branch', self)
         self.newBranch.triggered.connect(self.handleBranch)
-	self.table = ParseTable()
-	self.navbar = Navigator(self)
+        self.table = ParseTable()
+        self.navbar = Navigator(self)
 
     def handleBranch(self):
         branchInfo = self.newBranch.data()
         #print('create new tab:', branchInfo.toString())
-	self.table.setTableInfo(self.hit)
-	self.travler.findInTree(self.hit.element())
+        self.table.setTableInfo(self.hit)
+        self.travler.findInTree(self.hit.element())
 
     def contextMenuEvent(self, event):
         menu = self.page().createStandardContextMenu()
@@ -32,98 +32,99 @@ class WebView(QWebView):
             menu.addAction(self.newBranch)
         menu.exec_(event.globalPos())
     def urlHandler(self, url):
-	self.load(QUrl(url))
-	#TODO:
-	#Post load DOM tree parse
-	self.travler = DOMTravler(self.page().mainFrame())
+        self.load(QUrl(url))
+        #TODO:
+        #Post load DOM tree parse
+        self.travler = DOMTravler(self.page().mainFrame())
 
 class DOMTravler():
     def __init__(self, frame):
-	self.setFirst(frame.documentElement())
-	print(frame.toHtml().toAscii())
+    self.setFirst(frame.documentElement())
+    print(frame.toHtml().toAscii())
     def findInTree(self, element):
-	self.list = [self.first]
-	
-	#iterate Depth first and fill list
-	temp = self.first.firstChild() 
-	while ()
-	while (not temp.isNull()) and not (temp.__eq__(element)) :
-		self.list.append(temp)
-		temp = temp.firstChild()
+      self.list = [self.first]
 
-	if(temp.__eq__(element)):
-		print('element found!')
-		for el in self.list :
-			print(el.toInnerXml() + ' : ' + el.toOuterXml() + '\n')
-	else:
-		print('element not found!')
- 
+    #iterate Depth first and fill list
+      temp = self.first.firstChild()
+      while (not temp.isNull()) and not (temp.__eq__(element)):
+         while (not temp.isNull()) and not (temp.__eq__(element)):
+            self.list.append(temp)
+            temp = temp.firstChild()
+         temp = temp.nextSibling()
+
+      if(temp.__eq__(element)):
+          print('element found!')
+          for el in self.list :
+              print(el.toInnerXml() + ' : ' + el.toOuterXml() + '\n')
+      else:
+          print('element not found!')
+
     def setFirst(self, element):
-	self.first = element
-	
+      self.first = element
+
 
 
 class Navigator(QtGui.QWidget):
     def __init__(self, view):
-	super(Navigator, self).__init__()
-	self.setUrlHandlerObject(view)
+        super(Navigator, self).__init__()
+        self.setUrlHandlerObject(view)
         self.initUI()
 
     def initUI(self):
-	grid = QtGui.QGridLayout()
-	self.urlEdit = QtGui.QLineEdit()
-	self.goButton = QPushButton("GO!", self)
+        grid = QtGui.QGridLayout()
+        self.urlEdit = QtGui.QLineEdit()
+        self.goButton = QPushButton("GO!", self)
         self.goButton.clicked.connect(self.goButtonClicked)
-	grid.addWidget(self.goButton, 0, 1)
-	grid.addWidget(self.urlEdit, 0, 0)
-	grid.setSpacing(1)
-	self.setLayout(grid)
+        grid.addWidget(self.goButton, 0, 1)
+        grid.addWidget(self.urlEdit, 0, 0)
+        grid.setSpacing(1)
+        self.setLayout(grid)
 
     def goButtonClicked(self):
-	sender = self.sender()
-	self.view.urlHandler(self.urlEdit.text())
-	#TODO:
-	#imput validator
+        sender = self.sender()
+        self.view.urlHandler(self.urlEdit.text())
+    #TODO:
+    #input validator
     def setUrlHandlerObject(self, view):
-	self.view = view
+        self.view = view
 
 #this should hold interface form for parseing page selections
 class ParseTable(QtGui.QWidget):
     def __init__(self):
-	super(ParseTable, self).__init__()
-	self.initUI()
+        super(ParseTable, self).__init__()
+        self.initUI()
 
     def initUI(self):
-	grid = QtGui.QGridLayout()
-	self.link = QtGui.QLabel('URL:', self)
-	self.image = QtGui.QLabel('Image:', self)
-	self.element = QtGui.QLabel('Element:', self)
-	self.text = QtGui.QLabel('Text:', self)
-	self.title = QtGui.QLabel('Title:', self)
-	self.link.setWordWrap(True)
-	grid.addWidget(self.link, 1, 0)
-	grid.addWidget(self.image, 2, 0)
+        grid = QtGui.QGridLayout()
+        self.link = QtGui.QLabel('URL:', self)
+        self.image = QtGui.QLabel('Image:', self)
+        self.element = QtGui.QLabel('Element:', self)
+        self.text = QtGui.QLabel('Text:', self)
+        self.title = QtGui.QLabel('Title:', self)
+        self.link.setWordWrap(True)
+        grid.addWidget(self.link, 1, 0)
+        grid.addWidget(self.image, 2, 0)
         grid.addWidget(self.element, 3, 0)
         grid.addWidget(self.text, 4, 0)
         grid.addWidget(self.title, 5, 0)
         #grid.addWidget()
         #grid.addWidget()
 
-	self.setLayout(grid)
-#	self.lbl2 = QtGui.QLabel('ImageURL:', self)
-#	self.lbl3 = QtGui.QLabel('Element:', self)
-    
+        self.setLayout(grid)
+#        self.lbl2 = QtGui.QLabel('ImageURL:', self)
+#        self.lbl3 = QtGui.QLabel('Element:', self)
+
     def setTableInfo(self, hit):
         self.link.setText(QString(hit.linkUrl().toString()))
         self.image.setText(QString(hit.imageUrl().toString()))
-	lister = hit.element().attributeNames()
-	classes = hit.element().namespaceUri()
-	print(lister.__len__())
-	print(classes)
+        lister = hit.element().attributeNames()
+        classes = hit.element().namespaceUri()
+        print(lister.__len__())
+        print(classes)
         self.element.setText(classes)
         self.text.setText(hit.linkText())
-	self.title.setText(hit.title())
-	print(hit.linkUrl().toString())
+        self.title.setText(hit.title())
+        print(hit.linkUrl().toString())
 
 #TODO:
 #Need page middleware for highlighting parse selections
@@ -142,12 +143,12 @@ class VisualParseWindow(QtGui.QWidget):
 
         view = WebView()
         view.urlHandler('https://www.furaffinity.net/')
-	grid.addWidget(view.navbar, 0, 0)
-	grid.addWidget(view, 1, 0)
-	grid.addWidget(view.table, 1, 1)
-	self.setLayout(grid)
+        grid.addWidget(view.navbar, 0, 0)
+        grid.addWidget(view, 1, 0)
+        grid.addWidget(view.table, 1, 1)
+        self.setLayout(grid)
         self.setWindowTitle('Test2')
-	self.show()
+        self.show()
         #view.show()
 
 if __name__ == '__main__':
